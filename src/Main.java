017c2a189393af12775c28454a15969d2cc41cd1
kireev ks <kireev.ks@gmail.com>
2023-10-06 00:29:41 +0400

    changes

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,16 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int start = 100; // сумма на балансе до пополнения
+        int amount = 1000; // сумма пополнения
+        boolean bonus = amount / 100 = true;
+        if (amount > 1000 == true) {
+
+            System.out.println("итоговый баланс");
+            System.out.println(start + amount + bonus);
+
+        } else {
+            System.out.println("итоговый баланс");
+            System.out.println(start + amount);
+
+        }
     }
-}
